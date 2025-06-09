629c4d6384dd76665886015e819615299aa0fb89
murosyo01 <murozuka75@gmail.com>
2025-06-09 13:47:13 +0900

    feature/errorPage

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -52,9 +52,7 @@ public class EmployeeRepository {
 	public List<Employee> findAll() {
 		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,characteristics,dependents_count FROM employees";
 
-		List<Employee> developmentList = template.query(sql, EMPLOYEE_ROW_MAPPER);
-
-		return developmentList;
+		return template.query(sql, EMPLOYEE_ROW_MAPPER);
 	}
 
 	/**
@@ -69,9 +67,7 @@ public class EmployeeRepository {
 
 		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 
-		Employee development = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
-
-		return development;
+		return template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
 	}
 
 	/**
